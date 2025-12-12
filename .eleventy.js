e1b33fa2f9958abc0937a8cff3473975a8307ede
Jesús Manuel García Muñoz <hola@jesusmagamu.dev>
2025-12-12 09:35:44 +0100

    Unifies posts database

--- .eleventy.js
+++ .eleventy.js
@@ -8,20 +8,10 @@ const __dirname = path.dirname(__filename);
 
 export default function(eleventyConfig) {
   eleventyConfig.addCollection("posts", function() {
-    const postsDir = path.resolve(__dirname, "_includes/posts");
-    const files = fs.readdirSync(postsDir);
-
-    const posts = files
-      .filter(file => file.endsWith('.json'))
-      .map(file => {
-        const filePath = path.join(postsDir, file);
-        const content = fs.readFileSync(filePath, "utf8");
-        const posts = JSON.parse(content);
-
-        return posts;
-      })
-
-    return posts;
+    const dataPath = path.resolve(__dirname, "data/posts.json");
+    const content = fs.readFileSync(dataPath, "utf8");
+    const data = JSON.parse(content);
+    return data.posts;
   });
 
   eleventyConfig.addFilter("formatDate", (dateString, dateFormat) => {
